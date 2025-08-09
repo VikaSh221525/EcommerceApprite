fd5288100c5e9f59f53d04749f7d72d13652b140
vikassharma221525@gmail.com <vikassharma221525@gmail.com>
2025-08-09 16:19:53 +0530

    DeleteProductDeleteFromWishlist

--- src/store/actions/ProductAction.jsx
+++ src/store/actions/ProductAction.jsx
@@ -1,5 +1,8 @@
+import { data } from "react-router-dom";
 import { databases } from "../../lib/appwrite"
 import { loadproduct } from "../reducers/ProductSlice";
+import { Query } from "appwrite";
+import { removeFromWishlist } from "../reducers/WishlistSlice";
 
 
 export const asyncloadproducts = ()=> async (dispatch)=>{
@@ -42,8 +45,21 @@ export const asyncdeleteproduct = (productId) => async (dispatch, getState) =>{
             productId,
         )
 
+        const wishlistEntries = await databases.listDocuments(
+            '6894936d0026edd36555',
+            '689654eb002235267487',
+            [
+                Query.equal("productId", productId)
+            ]
+        );
+
+        for(const entry of wishlistEntries.documents){
+            await databases.deleteDocument('6894936d0026edd36555', '689654eb002235267487', entry.$id)
+        }
+
         const deletedProduct = getState().product.products.filter(p => p.$id !== productId);
         dispatch(loadproduct(deletedProduct))
+        dispatch(removeFromWishlist(productId))
 
     }catch(err){
         console.log("AsyncDeleteProductError: ", err);
