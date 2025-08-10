7773bcd987dfca1867b2ff1a0b85d5e37886e3a5
vikassharma221525@gmail.com <vikassharma221525@gmail.com>
2025-08-10 19:32:36 +0530

    ScrollToTop

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -13,23 +13,27 @@ import CategoryPage from '../components/CategoryPage'
 import Deals from '../components/Deals'
 import PageNotFound from '../pages/PageNotFound'
 import AuthWrapper from './AuthWrapper'
+import ScrollToTop from '../components/ScrollToTop'
 
 const Mainroutes = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="register" element={<Register/>} />
-            <Route path='login' element={<Login/>} />
-            <Route path='create-product' element={<AuthWrapper> <CreateProduct/> </AuthWrapper>} />
-            <Route path='products' element={<Product/>} />
-            <Route path='product/:id' element={<ProductDetails/>} />
-            <Route path='update-product/:id' element={ <AuthWrapper> <UpdateProduct/> </AuthWrapper>} />
-            <Route path='wishlist' element={<Wishlist/>} />
-            <Route path='cart' element={<Cart/>} />
-            <Route path='category/:categoryName' element={<CategoryPage/>} />
-            <Route path='deals' element={<Deals/>} />
-            <Route path='*' element={<PageNotFound/>} />
-        </Routes>
+        <>
+            <ScrollToTop/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="register" element={<Register />} />
+                <Route path='login' element={<Login />} />
+                <Route path='create-product' element={<AuthWrapper> <CreateProduct /> </AuthWrapper>} />
+                <Route path='products' element={<Product />} />
+                <Route path='product/:id' element={<ProductDetails />} />
+                <Route path='update-product/:id' element={<AuthWrapper> <UpdateProduct /> </AuthWrapper>} />
+                <Route path='wishlist' element={<Wishlist />} />
+                <Route path='cart' element={<Cart />} />
+                <Route path='category/:categoryName' element={<CategoryPage />} />
+                <Route path='deals' element={<Deals />} />
+                <Route path='*' element={<PageNotFound />} />
+            </Routes>
+        </>
     )
 }
 
