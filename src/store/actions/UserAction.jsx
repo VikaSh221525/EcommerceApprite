49ae70f7f53bfb623e9d02136ad4d877a85f055b
vikassharma221525@gmail.com <vikassharma221525@gmail.com>
2025-08-13 00:55:57 +0530

    FinalTOuches

--- src/store/actions/UserAction.jsx
+++ src/store/actions/UserAction.jsx
@@ -2,6 +2,8 @@ import axios from "axios"
 import { loginuser, logoutuser } from "../reducers/UserSlice";
 import { account, databases } from "../../lib/appwrite";
 import { Query } from "appwrite";
+import { clearCart } from "../reducers/CartSlice";
+import { clearWishlist } from "../reducers/WishlistSlice";
 
 const DB_ID = "6894936d0026edd36555";
 const COLLECTION_ID = "689495a2000eefa9b1ea";
@@ -39,6 +41,8 @@ export const asyncLogoutUser = () => async (dispatch)=>{
     try{
         await account.deleteSession('current');
         dispatch(logoutuser());
+        dispatch(clearCart());
+        dispatch(clearWishlist());
         localStorage.removeItem("currentUser");
     }catch(err){
         console.log("logout failed :", err);
